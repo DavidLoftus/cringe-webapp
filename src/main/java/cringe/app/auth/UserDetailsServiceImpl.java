0b3ff080f7904a3fdea1ee86f244493a31baed04
DavidLoftus <43099493+DavidLoftus@users.noreply.github.com>
2021-02-14 17:35:02 +0000

    User Details Service for spring-security

---
+++ src/main/java/cringe/app/auth/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package cringe.app.auth;
+
+import cringe.app.db.User;
+import cringe.app.db.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username) {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        }
+
+        return user;
+    }
+}
