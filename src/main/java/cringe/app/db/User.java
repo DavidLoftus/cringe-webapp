abdf4c7e9ed80014649700a3000848b9ea1ec6f6
DavidLoftus <43099493+DavidLoftus@users.noreply.github.com>
2021-02-15 15:36:52 +0000

    Revert "User Details Service for spring-security"
    This reverts commit 0b3ff080f7904a3fdea1ee86f244493a31baed04.

--- src/main/java/cringe/app/db/User.java
+++ src/main/java/cringe/app/db/User.java
@@ -1,16 +1,10 @@
 package cringe.app.db;
 
-import net.bytebuddy.dynamic.loading.InjectionClassLoader;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
 import javax.persistence.*;
-import java.util.Collection;
-import java.util.Collections;
 
 @Entity
 @Table(name="users")
-public class User implements UserDetails {
+public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -59,31 +53,6 @@ public class User implements UserDetails {
 
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Collections.emptyList();
     }
 
 }
