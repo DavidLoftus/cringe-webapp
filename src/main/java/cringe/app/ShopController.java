18ae2de78c014974b0a394f85204e61530e152d9
DavidLoftus <43099493+DavidLoftus@users.noreply.github.com>
2021-02-15 21:13:43 +0000

    Gamezzz

---
+++ src/main/java/cringe/app/ShopController.java
@@ -0,0 +1,86 @@
+package cringe.app;
+
+import cringe.app.db.Artifact;
+import cringe.app.db.ArtifactRepository;
+import cringe.app.db.Game;
+import cringe.app.db.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.sql.rowset.serial.SerialBlob;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+@Controller
+public class ShopController {
+
+    @Autowired
+    public ArtifactRepository artifactRepository;
+
+    @Autowired
+    public GameRepository gameRepository;
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("games", gameRepository.findAll());
+        return "index";
+    }
+
+    @GetMapping("/game/new")
+    public String newGame() {
+        return "new_game";
+    }
+
+    @GetMapping("/artifact/{id}/**")
+    @ResponseBody
+    public ResponseEntity<Resource> getArtifact(@PathVariable int id) throws SQLException {
+        final Artifact artifact = artifactRepository.findArtifactById(id);
+        if (artifact == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        Resource resource = new InputStreamResource(artifact.getData().getBinaryStream());
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_TYPE, artifact.getContentType())
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + artifact.getFileName() + "\"")
+                .body(resource);
+    }
+
+    @GetMapping("/play/{id}")
+    public String playGame(@PathVariable int id, Model model) {
+        Game game = gameRepository.findGameById(id);
+        if (game == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        model.addAttribute("game", game);
+        return "jsdos";
+    }
+
+    @PostMapping("/upload")
+    public String uploadFile(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) throws IOException, SQLException {
+        Artifact artifact = new Artifact();
+        artifact.setData(new SerialBlob(file.getBytes()));
+        artifact.setFileName(file.getOriginalFilename());
+        artifact.setContentType(file.getContentType());
+        artifact = artifactRepository.save(artifact);
+
+        Game game = new Game();
+        game.setTitle("idk tbh");
+        game.setArtifact(artifact);
+        game = gameRepository.save(game);
+
+        return "redirect:/play/" + game.getId();
+    }
+}
