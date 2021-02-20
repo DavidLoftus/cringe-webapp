f5dca0811aa5893d3f69a3cf0596bcd9c35f85a6
DavidLoftus <43099493+DavidLoftus@users.noreply.github.com>
2021-02-20 23:45:57 +0000

    Add debug admin account sa:password

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
 INSERT INTO "PUBLIC"."RELEASES" VALUES(1, 'application/x-zip-compressed', FILE_READ('./games/DOOM-@evilution.zip'), 'DOOM-@evilution.zip');
 INSERT INTO GAMES(TITLE, ARTIFACT_ID) VALUES ('Doom', 1);
+INSERT INTO USERS(USERNAME, PASSWORD, FULL_NAME, EMAIL) VALUES( 'sa', '$2y$12$GVtpMA.Cr7k20Xq9fEbwAO6BimfbxRJ/Y3yz3A0EoCxuOlX.XJltq', 's a', 's@a.sa' );
