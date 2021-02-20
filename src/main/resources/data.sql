1997aa1b01b3c507532eda9a27a9dd6652a268f6
DavidLoftus <43099493+DavidLoftus@users.noreply.github.com>
2021-02-20 23:41:30 +0000

    Add data.sql file to auto add games to database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO "PUBLIC"."RELEASES" VALUES(1, 'application/x-zip-compressed', FILE_READ('./games/DOOM-@evilution.zip'), 'DOOM-@evilution.zip');
+INSERT INTO GAMES(TITLE, ARTIFACT_ID) VALUES ('Doom', 1);
